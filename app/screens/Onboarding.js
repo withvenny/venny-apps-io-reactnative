d4cb2c8c880a8200ba50d81746ed7a2c848b6280
Al <adolphusnolan@gmail.com>
2021-12-13 09:53:39 -0600

    package.json naming added
    package.json naming added in order to produce app level directory pointers

--- app/screens/Onboarding.js
+++ app/screens/Onboarding.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Venny Labs I/O Application</Text>
+      <Text>Onboarding</Text>
       <StatusBar style="auto" />
     </View>
   );
